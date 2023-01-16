0f46a5d6b043c4f35a492b192c9430b87f66d1cc
dyihs <sydbgx@gmail.com>
2023-01-16 21:35:58 +0800

    使用clap构建CLI

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+pub mod cli;
 mod config;
 
 pub use config::{DiffConfig, DiffProfile, RequestProfile, ResponseProfile};
+
+#[derive(Debug, Clone,PartialEq, Eq)]
+pub struct ExtraArgs {
+    pub headers: Vec<(String, String)>,
+    pub query: Vec<(String, String)>,
+    pub body: Vec<(String, String)>,
+}
